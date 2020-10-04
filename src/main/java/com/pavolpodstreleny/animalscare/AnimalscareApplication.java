5d748e25388aeb838b54c910843394d143715bea
Pavol Podstreleny <palo.podstreleny@gmail.com>
2020-10-04 16:17:39 +0800

    Initial commit

---
+++ src/main/java/com/pavolpodstreleny/animalscare/AnimalscareApplication.java
@@ -0,0 +1,13 @@
+package com.pavolpodstreleny.animalscare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalscareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalscareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavolpodstreleny/animalscare/AnimalscareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavolpodstreleny.animalscare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalscareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
