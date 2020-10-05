d4dec4c650be2a55c730d4cc09fa341c2e512eaf
Pavol Podstreleny <palo.podstreleny@gmail.com>
2020-10-05 20:19:27 +0800

    Create Schedule data transform object

---
+++ src/main/java/com/pavolpodstreleny/animalscare/dto/ScheduleDTO.java
@@ -0,0 +1,46 @@
+package com.pavolpodstreleny.animalscare.dto;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import com.pavolpodstreleny.animalscare.entity.EmployeeSkill;
+
+public class ScheduleDTO {
+    private long id;
+    private List<Long> employeeIds;
+    private List<Long> petIds;
+    private LocalDate date;
+    private Set<EmployeeSkill> activities;
+
+    public List<Long> getEmployeeIds() {
+        return employeeIds;
+    }
+
+    public void setEmployeeIds(List<Long> employeeIds) {
+        this.employeeIds = employeeIds;
+    }
+
+    public List<Long> getPetIds() {
+        return petIds;
+    }
+
+    public void setPetIds(List<Long> petIds) {
+        this.petIds = petIds;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Set<EmployeeSkill> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(Set<EmployeeSkill> activities) {
+        this.activities = activities;
+    }
+}
